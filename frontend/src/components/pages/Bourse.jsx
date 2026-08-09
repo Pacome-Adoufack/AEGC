ecde4942b59231165b894f82cc482f456d2ebbf4
Landrytido <atla2804@student.eafc-uccle.be>
2026-08-09 07:00:36 +0200

    feat: mettre à jour la version de l'application à 2.2.1, remplacer Release.css par Price.css et ajouter le fichier Price.css avec des styles pour la page des prix

--- frontend/src/components/pages/Bourse.jsx
+++ frontend/src/components/pages/Bourse.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../styles/Release.css"; // On réutilise les mêmes styles
+import "../../styles/Price.css";
 import logo from "../../assets/logo.png";
 
 const bourseFiles = [
